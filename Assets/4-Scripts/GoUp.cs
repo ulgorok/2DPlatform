071fffe1cf9cdb0b7f8381f82fd2745dac0c9b59
BEHERAY Mark <beheraymark@gmail.com>
2025-05-15 13:03:23 +0200

    15.05

--- Assets/4-Scripts/GoUp.cs
+++ Assets/4-Scripts/GoUp.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class GoUp : MonoBehaviour
 {
     public Rigidbody2D _rig;
+    public float delay;
+
     // Start is called before the first frame update
     void Awake()
     {
         _rig = GetComponent<Rigidbody2D>();
         _rig.AddForce(new Vector2(0, 3f), ForceMode2D.Impulse);
     }
+
+    private void Start()
+    {
+        Invoke(nameof(EnableCollision), delay);
+    }
+
+    private void EnableCollision()
+    {
+        GetComponent<Collider2D>().enabled = true;
+    }
+
 }
